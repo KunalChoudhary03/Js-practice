a1641cbbd8676caa6307a115da351b9d6859f9ef
Kunal Choudhary <142143181+KunalChoudhary03@users.noreply.github.com>
2025-02-06 17:43:31 +0530

    javascript

---
+++ 01_LB/script.js
@@ -0,0 +1,25 @@
+// console.log("Hello World!");
+
+
+// function myFun(a,b){
+// console.log(a+b);
+// }
+// myFun(7,8);
+// myFun(7,8);
+// myFun(-1,-3);
+
+
+
+// function area(length, width){
+//     if(length <= 0 ){
+//         throw new RangeError("negative is not a length");
+//     }
+//     if(width <= 0 ){
+//         throw new RangeError("negative is not a width");
+//     }
+//     console.log("lenght is" ,length);
+//     console.log("width is" ,width);
+//     console.log("And area is ", length * width ); 
+//     }
+// area(8,9);
+// area(-1,99);
