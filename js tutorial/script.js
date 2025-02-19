facd5fd75d9003edf0c1070b82fec417ec7348f1
Kunal Choudhary <142143181+KunalChoudhary03@users.noreply.github.com>
2025-02-19 21:59:52 +0530

    javascript

---
+++ js tutorial/script.js
@@ -0,0 +1,132 @@
+// Age Category Message - Ask the user for their age. If they are
+// under 18, print "You are a minor." If they are between 18 and 60,
+// print "You are an adult." If they are above 60, print "You are a
+// senior citizen."
+// let ans = prompt("Enter your age");
+// if(ans<18){
+//     alert("you can minor ");
+// }
+// else if(ans>=18 && ans<60){
+//     alert("you can adult");
+// }
+// else{
+//     alert("you are sinear citizon");
+// }
+
+
+// Even or Odd Sum - Take two numbers from the user using
+// prompt(). If the sum of both numbers is even, print "Even Sum";
+// otherwise, print 'Odd Sum."
+// let ans = prompt("enter first number");
+// let ans1 = prompt("Enter second number");
+// let a = ans+ans1;
+// if(a%2==0){
+// alert("even sum");
+// }
+
+// else{
+// alert("odd sum");
+// }
+
+
+// Character Case Checker — Ask the user for a single character.
+// Check if it's uppercase, lowercase, or neither (not a letter).
+// let a = prompt("enter char");
+// let ans = a.charCodeAt(0);
+// if(ans>=65 && ans<=90){
+//  alert("uppercase");
+// }
+// else if(ans>=97 && ans<=122){
+//  alert("lower case");
+// }
+// else{
+//  alert("Not  a number ");
+// }
+
+
+// Largest6fThree NumbeiSUTake three numbers as input an
+// print the largest number among them without using
+// Math.max().
+// let a = prompt("enter first number");
+// let b = prompt("enter second number");
+// let c = prompt("enter third number");
+
+// if(a>b && a>c){
+//  alert(a + "is greater");
+// }
+// else if(b >a && b>c){
+//  alert(b + "is greater");
+// }
+// else{
+//  alert( c + " is grater");
+// }
+
+// Leap Year Checker — Ask the user for a year and determine if
+// it's a leao vear or not.
+// let a = prompt("enter a year");
+//        if((a%4 === 0 && a%100 !== 0) || (a%400 === 0)){
+//         alert("leap year");
+//        }
+//        else{
+//         alert("not a leap year");
+//        }
+
+
+// Simple Calculator — Ask the user for two numbers and an
+// operator (+, -, *, l). Perform the operation and display the result.
+// let a = prompt("Enter first number");
+// let b = prompt("Enter second number");
+// let sum = Number(a) + Number(b);
+// let mul = a*b;
+// let div = a/b;
+// let sub = a-b;
+// let modul = a%b;
+// alert( " sum is  "+sum  + "mul is  " + mul +    "  div is  " + div +" sub is " +sub +"    modul is    " + modul );
+
+
+
+
+// 7 . Positive, Negative, or Zero - Take a number input and check if it
+// is positive, negative, or zero.
+// let a = prompt("Enter a number");
+// if(a>0){
+//   alert("number is positive");
+// }
+// else if(a==0){
+// alert("number is zero");
+// }
+// else if(a<0){
+//   alert("number is negative");
+// }
+// else{
+//   alert("not a number");
+// }
+
+
+//Traffic Light System - Ask the user for a traffic light color (red,
+//     yellow, green). Print appropriate messages:
+//     Red: "Stop!"
+//     Yellow: "Get Ready!"
+//     Green: "
+
+// let a = prompt("Enter a color of traffic light ");
+//        if(a==="red"){
+//         alert("Please Stop");
+//        }
+//        else if(a==="yellow"){
+//         alert("Get ready!!");
+//        }
+//        else if(a==="green"){
+//         alert("Go");
+//        }
+//        else{
+//         alert("Enter the valid color");
+//        }
+
+//  Multiplication Table - Ask the user for a number and print its
+// multiplication table up to 10.
+
+// let a = prompt("Enter any number for table ");
+// for(let i=1;i<=10;i++){
+// alert(a + 'X' + i + "="+ a*i);
+// }
