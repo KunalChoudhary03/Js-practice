3c54464c28adc04ad5c29f7254969f74e689e417
Kunal Choudhary <142143181+KunalChoudhary03@users.noreply.github.com>
2025-02-21 21:54:29 +0530

    basic pracitce of js

---
+++ factorial/index.js
@@ -0,0 +1,44 @@
+// let arr = [1,2,3,5];
+// let n = 6;
+// for(let i=2;i<=n;i++){
+//     console.log(i)
+// }
+// const red = (n,i)=>{
+//     return a * b;
+// }
+// console.log(n.reduce(red));
+
+// function factorial(n) {
+//     if (n < 0) {
+//         return "Factorial not defined for negative numbers";
+//     }
+//     if (n === 0 || n === 1) {
+//         return 1;
+//     }
+//     let result = 1;
+//     for (let i = 2; i <= n; i++) {
+//         result *= i;
+//     }
+//     return result;
+// }
+
+// Example usage:
+// const number = 5;
+// console.log(`Factorial of ${number} is ${factorial(number)}`);
+
+
+//  function factorial(n){
+//     if(n<0){
+//         return "negative number "
+
+//     }
+//     if(n==0 || n==1){
+//         return 1;
+//     }
+//     let num = 1;
+//     for(let i=1;i<=n;){
+//         num *=i;
+//     }
+//     return num;
+//  }const number =5;
+//  console.log(`facorail of ${number} is ${factorail(number)}`)
