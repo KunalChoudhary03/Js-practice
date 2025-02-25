c76b9b17d691cba7b8638ccbb4f8dfac5ee37507
Kunal Choudhary <142143181+KunalChoudhary03@users.noreply.github.com>
2025-02-25 21:50:54 +0530

    js practice

---
+++ JS_02/script.js
@@ -0,0 +1,105 @@
+// javascript in five different way
+
+// console.log('Hello Javascript');
+// console.warn("Hello javscript");
+// console.error("Hello javscript");
+// console.info("Hello javscript");
+// console.table({name:"harsh",age:26});
+
+
+// console.log(35*2-(10/2) +7);
+
+// console.log(typeof("123"));
+// console.log(typeof(123));
+// console.log(typeof(true));
+// console.log(typeof(null));
+// console.log(typeof(undefined));
+
+
+//  var a = 12;
+//  var b = 13;
+// var temp;
+// console.log(" before a is"+ a);
+// console.log(" before b is "+b);
+// temp = a;
+// a = b;
+// b =temp;
+// console.log("a is"+ a);
+// console.log("b is "+b);
+
+// var a = 12 ;
+// var b =11;
+// [a,b] = [b,a];
+// console.log(a,b);
+
+// var a = 13;
+// var b = 11;
+// a = a+b;
+// b = a-b;
+// a = a-b;
+// console.log(a,b);
+
+
+// console.group("I will become dev");
+// console.log('a');
+// console.log('a');
+// console.log('a');
+// console.log('a');
+// console.log('a');
+
+// console.groupCollapsed('I will become dev');
+// console.log('a');
+// console.log('a');
+// console.log('a');
+// console.log('a');
+// console.log('a');
+// console.log('a');
+// console.groupEnd();
+
+// const obj ={
+//     name : "kunal",
+//     rollno : 12,
+//     mobileNo : 1234567890
+// }
+// console.log(obj);
+
+// Object.freeze(obj); //this will not chnage show error
+// obj.name = "kuna";
+// console.log(obj);
+ 
+
+// let a = "55";
+// // num = Number(a);
+// // num = +a;
+// // num = parseInt(a);
+// console.log(num);
+// console.log(typeof num);
+// console.log(typeof a);
+
+// let str = "Javascript";
+// // if(str.indexOf("script") === -1){
+// //     console.log(false);
+    
+// // }
+// // else{
+// //     console.log(true);
+
+// // }
+// if(str.search("script") === -1){
+//     console.log(false);
+    
+// }
+// else{
+//     console.log(true);
+
+// }
+
+
+
+
+
+
+
+
+
+
