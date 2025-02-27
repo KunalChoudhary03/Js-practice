119be43072905e06cf7231609f1c32237912ca1b
Kunal Choudhary <142143181+KunalChoudhary03@users.noreply.github.com>
2025-02-27 21:49:42 +0530

    javascript

---
+++ JS-03/script.js
@@ -0,0 +1,54 @@
+// var array = [1,2,3,4];
+// var ans = array.map(function(value){
+//     return 12;
+// })
+
+// array.forEach(value => {
+//     console.log(value);
+    
+// });
+
+// var ans = array.filter(function(value){
+//     return true;
+// })
+// var ans = array.filter(function(value){
+//     return value === 3;
+// })
+// var ans = array.filter(function(value){
+//     return value === 3;
+// })
+
+// var arr = [1,2,3,4];
+// let ans = arr.reduce(function(accumulator, key){
+//     return accumulator + key; 
+// },0);
+// console.log(ans);
+
+// let i=10;
+// while(i>0){
+//     console.log(i);
+//     i--;
+// }
+
+// let i = 3;
+// while(i<31){
+//     console.log(i);
+//     i+=3;
+// }
+// var sum = 0;
+
+// for(let i=1;i<=100; i++){
+//   sum += i;
+// }
+// console.log(sum);
+
+// var str = "kunal";
+// for(let i of str){
+//     console.log(i);
+    
+// }
+
+// let arr = [1,1,1,2,3,4,5,6,7,8,9,9,9];
+// let arr2 = [...new Set(arr)];
+// console.log(arr2);
+
