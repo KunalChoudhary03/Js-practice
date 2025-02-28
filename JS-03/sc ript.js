8a3bfe3e6f03ae1382d8d8a7a2dacaa0137b5d43
Kunal Choudhary <142143181+KunalChoudhary03@users.noreply.github.com>
2025-02-28 21:43:00 +0530

    js

---
+++ JS-03/sc ript.js
@@ -0,0 +1,29 @@
+// let arr = [1,2,3,4,5];
+// for(let i = arr.length; i >= 0 ;i--){
+//     console.log(arr[i]);
+// }
+
+// second largest number in array
+// var arr = [1,2,3,4,5,4,6,7,8,89];
+// console.log(
+// [...new Set(arr)].sort(function(a,b){
+//     return b-a;
+// })[1]);
+// var ans = arr.sort((a,b) => b - a);
+// console.log(ans);
+
+
+// let arr = [1,2,3,4,5,6,67];
+// var arr2 = [];
+// for(let i=arr.length-1;i>=0;i--){
+//     arr2.push(arr[i]);
+// }
+// console.log(arr2);
+
+// // count of number ;
+// let arr = [3,3,4,5,6,7,8,8,7,6,];
+// let obj ={};
+// arr.forEach(function (val){
+//     obj[val] === undefined ? (obj[val] = 1) : obj[val]++;
+// });
+// console.log(obj);
