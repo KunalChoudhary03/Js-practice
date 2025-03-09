477883c70773edad98dc00a292fb8f3cdb2f9a96
Kunal Choudhary <142143181+KunalChoudhary03@users.noreply.github.com>
2025-03-09 23:22:05 +0530

    js

---
+++ JS_02/script_01.js
@@ -0,0 +1,49 @@
+// compound intrest
+// let p = Number(prompt("Enter principle"));
+// let r = Number(prompt("Enter rate"));
+// let t = Number(prompt("Enter time"));
+// console.log(p*Math.pow(1 + r/100, t) -p);
+// OTP generatore
+// let a = Math.floor(Math.random()*9000 + 1000);
+// console.log(a);
+
+// let a  = Number(prompt("Enter first number"));
+// let b  = Number(prompt("Enter second number"));
+// let c  =Number(prompt("Enter third number"));
+// let s = a+b+c/2;
+// if(a+b<=c || b+c<=a || c+a<=b){
+    //     console.log("Triangle not possible");
+    // }
+    // else{
+        //     console.log("Triangle possible");
+        //     console.log(Math.sqrt(s*(s-a)*(s-b)*(s-c)));
+        // }
+        
+        
+        // let r =Number(prompt("Enter a number"));
+        // alert(2*Math.PI*r);
+
+
+// let a  = Number(prompt("Enter first number"));
+// let b  = Number(prompt("Enter second number"));
+// if(a>b){
+//     alert(a + "is greater");
+// }
+// else if(a==b){
+//     alert("Both are equal");
+// }
+// else{
+//     alert(b + "is greater");
+// }
+        
+let a  = Number(prompt("Enter first number"));
+let b  = Number(prompt("Enter second number"));
+if(a%b == 0){
+    alert("Number is even");
+}
+else{
+    alert("Number is odd");
+}
+        
+        
+        
