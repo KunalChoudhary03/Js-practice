e6f6f97d5649af1e8b95a51ad638785549007cd7
Kunal Choudhary <142143181+KunalChoudhary03@users.noreply.github.com>
2025-03-11 22:24:42 +0530

    js

---
+++ script_02.js
@@ -0,0 +1,64 @@
+// let a  =4;
+// if((isNaN)(a)){
+//     console.log('invaild input');
+// }
+// else{
+// for(let i=1 ; i<=a ; i++){
+//     console.log("hello world");
+// }
+// }
+
+// let n = Number(prompt("Enter the number"));
+// for(let i=1;i<=n;i++){
+//     console.log(i);
+// }
+
+// let A = Number(prompt("Enter the number"));  
+// for(let i=A;i>=1;i--){
+//     console.log(i);
+// }
+// table
+//  let A = Number(prompt("Enter the number for table"));
+//  for(let i = 1;i<=10;i++){
+//         console.log(`${A}*${i}=${A*i}`);
+
+//  }
+
+// let n = 5;
+// let sum = 0;
+// for(let i=1;i<=n;i++){
+//     sum += i;
+// }
+// console.log(sum);
+
+// let n = 10;
+// let sum = 1;
+// for(let i=1;i<=n;i++){
+//     sum *= i;
+// }
+// console.log(sum);
+
+// let n = Number(prompt("Enter the number"));
+// let even = 0 , odd = 0;
+// for(let i=1;i<=n;i++){
+//   if(i%2==0){
+//     even += i;
+//   }
+//   else{
+//     odd += i;
+//   }
+// }
+// console.log(`Sum of even number is ${even}`);
+// console.log(`Sum of odd number is ${odd}`);
+
+
+let n = 10;
+for(let i=1;i<=n;i++){
+    if(n%i==0){
+        console.log(i);
+    }
+}
+
+
+
+
