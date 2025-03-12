599f73815d2f3f4477315d9ad78d80386b3e454c
Kunal Choudhary <142143181+KunalChoudhary03@users.noreply.github.com>
2025-03-12 23:28:32 +0530

    JS

---
+++ JS-03/script_03.js
@@ -0,0 +1,61 @@
+// let n =10;
+// for(let i=1;i<=n;i++){
+//     if(i==7){
+//         break;
+//     }
+//     console.log(i);
+    
+// }
+
+// let n =10;
+// for(let i=1;i<=n;i++){
+//     if(i==7){
+//         continue;
+//     }
+//     console.log(i);
+    
+// }
+
+// let n =10;
+// let i = 1;
+// while(i<=n){
+//     console.log("Hello World!");
+//     i++;
+// }
+
+// let n =12345;
+// let sum = 0;
+// while(n>0){
+//     rem = n%10;
+//     sum += rem;
+//     n = Math.floor(n/10);
+// }
+// console.log(sum);
+
+
+// let num = 23456;
+// let rev = 0;
+
+// while (num > 0) {
+//     let rem = num % 10;     
+//     rev = rev * 10 + rem;   
+//     num = Math.floor(num / 10);
+// }
+
+// console.log(rev);
+
+
+// let n = 4;
+// let copy = n;
+// let sq = n*n ;
+// let count = 0;
+// while(n>0){
+//     count++;
+//     n = Math.floor(n/10);
+// }
+// if(sq%Math.pow(10,count) === copy){
+//     console.log("Automorphic Number");
+
+// }else{
+//     console.log("Not an Automorphic Number");
+// }
