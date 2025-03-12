be77d9932a55f7b345c19a5f3da7cd63f254c843
Kunal Choudhary <142143181+KunalChoudhary03@users.noreply.github.com>
2025-03-12 21:20:26 +0530

    script_01.js

--- JS_02/script_01.js
+++ JS_02/script_01.js
@@ -36,14 +36,114 @@
 //     alert(b + "is greater");
 // }
         
-let a  = Number(prompt("Enter first number"));
-let b  = Number(prompt("Enter second number"));
-if(a%b == 0){
-    alert("Number is even");
-}
-else{
-    alert("Number is odd");
-}
-        
+// let a  = Number(prompt("Enter first number"));
+// let b  = Number(prompt("Enter second number"));
+// if(a%b == 0){
+//     alert("Number is even");
+// }
+// else{
+//     alert("Number is odd");
+// }
+
+// let a  =4;
+// if((isNaN)(a)){
+//     console.log('invaild input');
+// }
+// else{
+// for(let i=1 ; i<=a ; i++){
+//     console.log("hello world");
+// }
+// }
+
+// let n = Number(prompt("Enter the number"));
+// for(let i=1;i<=n;i++){
+//     console.log(i);
+// }
+
+// let A = Number(prompt("Enter the number"));  
+// for(let i=A;i>=1;i--){
+//     console.log(i);
+// }
+// table
+//  let A = Number(prompt("Enter the number for table"));
+//  for(let i = 1;i<=10;i++){
+//         console.log(`${A}*${i}=${A*i}`);
+
+//  }
+
+// let n = 5;
+// let sum = 0;
+// for(let i=1;i<=n;i++){
+//     sum += i;
+// }
+// console.log(sum);
+
+// let n = 10;
+// let sum = 1;
+// for(let i=1;i<=n;i++){
+//     sum *= i;
+// }
+// console.log(sum);
+
+// let n = Number(prompt("Enter the number"));
+// let even = 0 , odd = 0;
+// for(let i=1;i<=n;i++){
+//   if(i%2==0){
+//     even += i;
+//   }
+//   else{
+//     odd += i;
+//   }
+// }
+// console.log(`Sum of even number is ${even}`);
+// console.log(`Sum of odd number is ${odd}`);
+
+// factores of number
+// let n = 13;
+// for(let i=1;i<=n/2;i++){
+//     if(n%i==0){
+//         console.log(i);
+//     }
+// }
+// console.log(n);
+
+// let n = 2;
+// let isPrime = true;
+// for(let i=2;i<=n/2;i++){
+//   if(n % i  == 0){
+//     isPrime = false;
+//     break;
+//   }
+  
+// }
+// if(isPrime){
+//     console.log('prime');
+    
+//   }
+//   else{
+//     console.log('not prime');
+    
+//   }
+
+
+// let n = 2;
+// let isPrime = isPrimeFun(n);
+// if(isPrime)console.log('prime');
+// else console.log('not prime');
+ 
+// function isPrimeFun(n){
+//     if(n<=1) return false;
+//     if(n==2) return true;
+//     if(n%2==0) return false;
+//     for(let i=3;i<=Math.sqrt(n);i+=2){
+//         if(n%i==0) return false;
+//     }
+//     return true;
+// }
+
+
+// let a = 2;
+// let b = 5;
+// console.log(Math.pow(a,b));
         
         
