816c4fc10e7c1ad1aaa427eef706be88b294ed7d
Kunal Choudhary <142143181+KunalChoudhary03@users.noreply.github.com>
2025-03-12 23:49:50 +0530

    script_03.js

--- JS-03/script_03.js
+++ JS-03/script_03.js
@@ -59,3 +59,29 @@
 // }else{
 //     console.log("Not an Automorphic Number");
 // }
+
+
+ // let n = 5;
+//  switch(n){
+//     case 1 : 
+//     console.log('one');
+//     break;
+//     case 2:
+//     console.log('two');
+//     break;
+        
+//     case 3:
+//     console.log('three');
+//     break;
+        
+//     case 4:
+//     console.log('four');
+//     break;
+        
+//     case 5:
+//     console.log('five');
+//     break;
+
+//     default: console.log("default");
+//     break;
+//  }
