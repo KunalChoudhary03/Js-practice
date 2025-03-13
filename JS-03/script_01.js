6c54be9539de2ae7068209216e22f984f1bd3a69
Kunal Choudhary <142143181+KunalChoudhary03@users.noreply.github.com>
2025-03-13 22:38:38 +0530

    js

---
+++ JS-03/script_01.js
@@ -0,0 +1,61 @@
+// let a = 2025;
+// if(a%4==0 && a%100!=0 || (a%400==0)){
+//     console.log('leap year');
+// }
+// else{
+//     console.log('not a leap year');
+    
+// }
+
+// let amount = 6000;
+// if(amount>=0 && amount<=5000){
+//     console.log(amount);
+// }
+// else if(amount>5000 && amount<=7000){
+//     console.log(amount - (5*amount)/100);   
+// }
+// else if(amount>7000 && amount<=9000){
+//     console.log(amount - (10*amount)/100);   
+// }
+// else if(amount>9000){
+//     console.log(amount - (20*amount)/100);   
+// }
+// else{
+//      console.log("invalid input");}
+
+
+//  let unit = 700;
+//  let amount = 0;
+//  if(unit>0 && unit <=100){
+    //     amount = unit * 4.2;
+    
+//  }
+//  else if(unit>100 && unit <=200){
+//     amount = (100*4.2) + (unit-100) *6; 
+//  }
+//  else if(unit>200 && unit <=400){
+    //     amount = (100*4.2) + (unit*6)(unit-200) *8; 
+    //  }
+    //  else if(unit>400){
+        //     amount = (100*4.2) + (unit*6) +(unit*6) + (unit-400) *13; 
+        //  }
+        //  console.log(amount);
+        
+        
+// let unit = 700;
+// let amount = 0;
+// if(unit>400){
+//      amount = (unit -400)  * 13;
+//      unit = 400;
+// }
+// if(unit>200 && unit <= 400){
+//   amount  = amount + (unit-200)*8;
+//   unit = 200;
+// }
+// if(unit>100 && unit <= 200){
+//   amount  = amount + (unit-100)*6;
+//   unit = 100;
+// }
+// amount = amount + unit*4.2;
+// console.log(amount);
+
