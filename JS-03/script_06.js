a8131b96674e16983c6895c79de0edc552bc6904
Kunal Choudhary <142143181+KunalChoudhary03@users.noreply.github.com>
2025-03-17 22:02:41 +0530

    * pattern printing

---
+++ JS-03/script_06.js
@@ -0,0 +1,106 @@
+// Pattern printing
+
+// for(let i =1;i<=5;i++){
+//     console.log('*');
+// }
+
+//Not good approch
+// for(let i =1;i<=5;i++){
+//         process.stdout.write("*");
+    
+// }
+// console.log();
+// for(let i =1;i<=5;i++){
+//         process.stdout.write("*");
+    
+// }
+// console.log();
+// for(let i =1;i<=5;i++){
+//         process.stdout.write("*");
+    
+// }
+// console.log();
+// for(let i =1;i<=5;i++){
+//         process.stdout.write("*");
+    
+// }
+// console.log();
+// for(let i =1;i<=5;i++){
+//         process.stdout.write("*");
+    
+// }
+// console.log();
+
+
+// for(let i =1 ;i<=5;i++){
+//     for(let j =1 ;j<=5;j++){
+//     process.stdout.write("* ");
+//     }
+//     console.log();
+    
+// }
+
+// const prompt = require("prompt-sync")()
+// let n = prompt("Enter a number");
+// console.log(n);
+
+
+// const prompt = require("prompt-sync")()
+// let n = prompt("Enter a number");
+// for(let i =1;i<=n;i++){
+//     for(j=1;j<=n;j++){
+
+//         process.stdout.write("*");
+//     }
+//     console.log();
+    
+// }
+
+
+// const prompt = require("prompt-sync")()
+// let n = prompt("Enter a number");
+// for(let i =1;i<=n;i++){
+//     for(j=1;j<=i;j++){
+
+//         process.stdout.write("*");
+//     }
+//     console.log();
+    
+// }
+
+// const prompt = require("prompt-sync")()
+// let n = prompt("Enter a number");
+// for(let i =1;i<=n;i++){
+//     for(j=1;j<=i;j++){
+//         process.stdout.write(j+"");
+//     }
+//     console.log();
+// }
+
+// const prompt = require("prompt-sync")()
+// let n = prompt("Enter a number");
+// for(let i =1;i<=n;i++){
+//     for(j=1;j<=i;j++){
+//         process.stdout.write(String.fromCharCode(64 + j) + " ");
+//     }
+//     console.log();
+// }
+
+// const prompt = require("prompt-sync")()
+// let n = prompt("Enter a number");
+// for(let i = 1;i<=n;i++){
+//     for(j=1;j<=n-i+1;j++){
+//         process.stdout.write("* ");
+//     }
+//     console.log();
+// }
+
+// const prompt = require("prompt-sync")();
+// let n = prompt("Enter a number");
+// for(let i =1;i<n;i++){
+//     for(let j=1;j<=n-i+1;j++){
+//         process.stdout.write(String.fromCharCode(64 + j)+ "");
+//     }
+//     console.log();
+    
+// }
