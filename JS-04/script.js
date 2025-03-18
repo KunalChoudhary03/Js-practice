5e6b963dff681ded72590011315f4efbb6f6a792
Kunal Choudhary <142143181+KunalChoudhary03@users.noreply.github.com>
2025-03-18 22:03:15 +0530

    JS_04

---
+++ JS-04/script.js
@@ -0,0 +1,59 @@
+// for(let i=0;i<=1000;i++){
+//     process.stdout.write(String.fromCharCode(i))
+// }
+// const prompt = require("prompt-sync")();
+// let n = prompt("Enter a number");
+// for(let i =1;i<=n;i++){
+//     for(let j=1;j<=n-i;j++){
+//        process.stdout.write("  ");
+//     }
+//     for(let j=1;j<=i;j++){
+//         process.stdout.write("* ")
+//     }
+//     console.log();
+    
+// }
+
+// const prompt = require("prompt-sync")();
+// let n = prompt("Enter a number");
+// for(let i =1;i<=n;i++){
+    //     for(let j=1;j<=n-i;j++){
+        //        process.stdout.write(" ");
+        //     }
+        //     for(let j=1;j<=i;j++){
+            //         process.stdout.write("* ")
+            //     }
+            //     console.log();
+            // }
+
+// const prompt = require("prompt-sync")();
+// let n = Number(prompt("Enter a number"));
+// for(let i =1;i<=n;i++){
+    //     for(let j =1;j<=n;j++){
+        //         if(i==j || i+j === n+1){
+            //         process.stdout.write("* ");
+            //     }
+            //     else{
+                
+            //         process.stdout.write("  ");
+            //         }
+            //     }
+            //     console.log();
+            // }
+
+// const prompt = require("prompt-sync")();
+// let n = Number(prompt("Enter a number: "));
+
+// for (let i = 1; i <= n; i++) {
+//     for (let j = 1; j <= (n * 2) - 1; j++) {
+//         if (i == j || i + j === n * 2) {
+//             process.stdout.write("* ");
+//         } else {
+//             process.stdout.write("  "); 
+//         }
+//     }
+//     console.log(); 
+// }
+
+// let arr = ["kunal" , 4, 44, true];
+// console.log(arr);
