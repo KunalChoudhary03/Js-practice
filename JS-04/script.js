fd40953bf2e4d5c110b0d9842b84d8fbedbdc793
Kunal Choudhary <142143181+KunalChoudhary03@users.noreply.github.com>
2025-03-19 12:25:54 +0530

    script.js

--- JS-04/script.js
+++ JS-04/script.js
@@ -72,3 +72,28 @@
 //     console.log();
     
 // }
+
+
+// const prompt = require("prompt-sync")();
+// let n = prompt("Enter a number");
+// for(let i =1;i<=n;i++){
+//     for(let j=1;j<=n-i;j++){
+//        process.stdout.write(" ");
+//     }
+//     for(let j=1;j<=i;j++){
+//         process.stdout.write("* ")
+//     }
+//     console.log();
+    
+// }
+// for(let i=n-1;i>=1;i--){
+//     for(let j=1;j<=n-i;j++){
+//         process.stdout.write(" ");
+//     }
+//     for(let j=1;j<=i;j++){
+//         process.stdout.write("* ");
+//     }
+//     console.log();
+    
+// }
+
