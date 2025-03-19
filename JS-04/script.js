5ab949dbf816be870e193aa48c1091bb6f84c12e
Kunal Choudhary <142143181+KunalChoudhary03@users.noreply.github.com>
2025-03-19 12:10:19 +0530

    script.js

--- JS-04/script.js
+++ JS-04/script.js
@@ -57,3 +57,18 @@
 
 // let arr = ["kunal" , 4, 44, true];
 // console.log(arr);
+
+
+// let n =5;
+// for(let i=1;i<=n;i++){
+//     for(let j=1;j<=n;j++){
+//         if(i===1 || i === n || j === 1 || j === n){
+//             process.stdout.write("* ");
+//         }
+//         else{
+//             process.stdout.write("  ");
+//         }
+//     }
+//     console.log();
+    
+// }
