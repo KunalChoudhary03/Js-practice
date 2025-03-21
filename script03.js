e8083eed8d87f9e867f096e56447d73ed06293b3
Kunal Choudhary <142143181+KunalChoudhary03@users.noreply.github.com>
2025-03-21 22:07:09 +0530

    js

---
+++ script03.js
@@ -0,0 +1,71 @@
+// let arr = [28,6,74,48,84,84]
+// let max = Math.max(arr[0],arr[1]);
+// let sMax = Math.min(arr[0],arr[1]);
+// for(let i=2;i<arr.length;i++){
+//     if(arr[i]>max){
+//         sMax = max ;
+//         max = arr[i]
+//     }
+//     else if(arr[i]> sMax && arr[i] != max){
+//         sMax = arr[i] ;
+//     }
+
+// }
+// console.log("Second max" + sMax);
+
+
+
+// let arr = [1,3,4,5,6,7,8];
+// let temp = new Array(arr.length);
+// let i=0;
+// for(let j = arr.length-1;j>=0;j--){
+    // temp[i] = arr[j];
+    // i++;
+    // }
+    // console.log(temp);
+    
+    
+// let arr = [1,3,4,5,6,7,8];
+// let i=0, j = arr.length-1;
+
+// while(i<j){
+//     let temp = arr[i];
+//     arr[i] = arr[j];
+//     arr[j] = temp;
+//     i++;
+//     j--;
+// }
+// console.log(arr);
+
+
+// let arr = [1,0,1,0,1,0,1];
+// let i=0,j=0;
+// while(i<arr.length){
+//     if(arr[i] == 0){
+//         let temp = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = temp;
+//         j++;
+     
+//     }
+//     i++;
+// }
+// console.log(arr);
+
+// let arr = [1,3,5,7,9,4];
+// temp = arr[0];
+// for(let i=0;i<arr.length-1;i++){
+
+//     arr[i] = arr[i+1];
+// }
+// arr[arr.length-1] = temp;
+// console.log(arr);
+
+// let arr = [1,3,5,7,9,4];
+// temp = arr[arr.length-1];
+// for(let i=arr.length-1;i>0;i--){
+//     arr[i] = arr[i-1];
+// }
+// arr[0] = temp;
+// console.log(arr);
+
