08635f9e27de62ab37e463d22bb1b3138a4f3679
Kunal Choudhary <142143181+KunalChoudhary03@users.noreply.github.com>
2025-03-28 15:38:41 +0530

    Set timeout and SetInterval

---
+++ Dom/script_02.js
@@ -0,0 +1,38 @@
+// console.log("Hello1");
+// setTimeout(function(){
+//     console.log("Hello2");
+// },3000)
+// console.log("Hello3");
+
+
+// let button = document.querySelector("button");
+// let flage = 0;
+// let h5 =  document.querySelector("h5");
+// button.addEventListener("click",function(){
+   
+//     if(flage === 0){
+//         h5.innerHTML = "Request sending....";
+//        h5.style.color = "orange";
+//     setTimeout(() => {
+//         button.textContent = "Remove Friend";
+//         h5.textContent = "Friend";
+//         h5.style.color = "green"
+//         flage = 1;
+//     }, 2000);
+//     }
+//     else{
+//         button.textContent = "Add friend ";
+//         h5.textContent = "Stranger";
+//         h5.style.color = "red";
+//         flage = 0;
+// }
+   
+// })
+
+
+// var a = 0;
+// setInterval(() => {
+//     console.log("value of a is " + a);
+//     a++;
+    
+// },2000);
