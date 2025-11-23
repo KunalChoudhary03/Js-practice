7e82516ce8efc11427ee631abc0195e8a1b405c2
Kunal Choudhary <142143181+KunalChoudhary03@users.noreply.github.com>
2025-11-23 18:26:16 +0530

    Chatgpt in our local machine

---
+++ index.js
@@ -0,0 +1,30 @@
+import 'dotenv/config'
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import promptSync from "prompt-sync";
+const prompt = promptSync();
+const ai = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+async function main(message) {
+  const model  =  ai.getGenerativeModel({
+    model: "gemini-2.5-flash",
+    systemInstruction: `
+      You are a helpful AI assistant.
+      Always respond in **maximum 150 words**.
+      Keep answers simple, clear and beginner-friendly.Tell me how can i help you!!😊.
+    `
+  });
+ const response = await model.generateContent(`${message}`);
+ console.log(response.response.text());
+
+}
+async function lopping(){
+   while(true){
+     let ask = prompt("Enter text :-")
+  if(ask.toLowerCase() =="exit"){
+   console.log("Byee!!!");
+   break;
+  }
+   await main(`${ask}`);
+ }
+}
+lopping();
